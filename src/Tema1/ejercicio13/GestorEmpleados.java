b0b080da7269aaa2ddc65ce8edb019830ea3c929
GudraFree <perig1607@hotmail.com>
2017-10-09 11:37:51 +0200

    Progreso ej13 v2. Ejemplo añadido

--- src/Tema1/ejercicio13/GestorEmpleados.java
+++ src/Tema1/ejercicio13/GestorEmpleados.java
@@ -11,9 +11,9 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -37,6 +37,9 @@ public class GestorEmpleados {
         String ciudad;
         float salario;
         byte control;
+        void mostrar() {
+            System.out.println("Empleado #"+id+"\n\tNombre: "+nombre+" "+ape1+" "+ape2+"\n\tDepartamento: "+depart+"\n\tCiudad: "+ciudad+"\n\tSalario: "+salario);
+        }
     }
     
     //clase para gestionar el display y las opciones del menú del gestor
@@ -48,7 +51,7 @@ public class GestorEmpleados {
             boolean opcionInvalida;
             int opcion;
             do {
-                System.out.println("Gestor de usuarios. Opciones disponibles:\n\n\t1. Alta\nºt2. Baja\n\t3. Modificación\n\t4. Consulta\n\t5. Salir\n\nSeleccione una opción: ");
+                System.out.println("Gestor de usuarios. Opciones disponibles:\n\n\t1. Alta\n\t2. Baja\n\t3. Modificación\n\t4. Consulta\n\t5. Salir\n\nSeleccione una opción: ");
                 opcion = sc.nextInt();
                 opcionInvalida = opcion<1 || opcion>NOPCIONES; //si se introduce una opción no contemplada es inválida
                 if(opcionInvalida) System.out.println("Error, opción inválida\n\n"); //mensaje de error si opción inválida
@@ -72,11 +75,13 @@ public class GestorEmpleados {
         Menu.opcionMenu opcion;
         File empleados = new File(new File(System.getProperty("user.home"),"Desktop"),"empleados.txt");
         File indice = new File(new File(System.getProperty("user.home"),"Desktop"),".indice"); //archivo oculto con la cuenta de índices de empleados, que son únicos
-        try {
-            dis = new DataInputStream(new BufferedInputStream(new FileInputStream(empleados)));
-            dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(empleados)));
-        } catch (FileNotFoundException e) {}
+//        try {
+//            dis = new DataInputStream(new BufferedInputStream(new FileInputStream(empleados)));
+//            dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(empleados)));
+//        } catch (FileNotFoundException e) {}
         while((opcion=Menu.menu())!= Menu.opcionMenu.salir) {
+            ArrayList<Empleado> listaEmpleados;
+            Empleado empleadoAux;
             switch (opcion) {
                 case alta:
                     //creamos fichero de empleados si este no existe
@@ -153,6 +158,7 @@ public class GestorEmpleados {
                         dos.writeUTF(empleado.ape2);
                         dos.writeUTF(empleado.depart);
                         dos.writeUTF(empleado.ciudad);
+                        dos.writeFloat(empleado.salario);
                         dos.writeByte(empleado.control);
                         dos.close();
                     } catch (IOException e) {}
@@ -171,6 +177,184 @@ public class GestorEmpleados {
                     } catch (IOException e) {}
                     
                     //fin de alta
+                    break;
+                case baja: 
+                    if(!empleados.exists()) {
+                        System.out.println("Error, no existen empleados. Por favor realice al menos un alta antes de realizar una baja.\n");
+                        break;
+                    }
+                    
+                    //leemos el archivo y almacenamos sus datos en una lista de empleados
+                    listaEmpleados = new ArrayList();
+                    try {
+                        dis = new DataInputStream(new BufferedInputStream(new FileInputStream(empleados)));
+                        while(dis.available()>0) {
+                            empleadoAux = new Empleado();
+                            empleadoAux.id = dis.readInt();
+                            empleadoAux.nombre = dis.readUTF();
+                            empleadoAux.ape1 = dis.readUTF();
+                            empleadoAux.ape2 = dis.readUTF();
+                            empleadoAux.depart = dis.readUTF();
+                            empleadoAux.ciudad = dis.readUTF();
+                            empleadoAux.salario = dis.readFloat();
+                            empleadoAux.control = dis.readByte();
+                            listaEmpleados.add(empleadoAux);
+                            System.out.println("Debug: Empleado "+empleadoAux.id+" leído");
+                        }
+                        dis.close();
+                    } catch (IOException e) {} 
+                    
+                    System.out.println("Introduzca el id del empleado a dar de baja:");
+                    int idBaja = sc.nextInt();
+                    sc.nextLine(); //limpiamos escaner
+                    
+                    boolean exitoBaja = false; //booleano de control para comprobar si se ha podido realizar la baja
+                    
+                    // volvemos a escribir todos los empleados en el fichero, cambiando antes el byte de control del empleado que damos de baja
+                    try {
+                        dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(empleados, false))); //no utilizamos el append pues queremos sobreescribir el fichero
+                        for(Empleado emp : listaEmpleados) {
+                            if(emp.id==idBaja) {
+                                emp.control = BORRADO;
+                                exitoBaja = true; //si el id es válido entrará a este if, realiza la baja y se confirma el éxito de la operación
+                                System.out.println("Baja del empleado "+emp.id+" realizada con éxito");
+                            }
+                            dos.writeInt(emp.id);
+                            dos.writeUTF(emp.nombre);
+                            dos.writeUTF(emp.ape1);
+                            dos.writeUTF(emp.ape2);
+                            dos.writeUTF(emp.depart);
+                            dos.writeUTF(emp.ciudad);
+                            dos.writeFloat(emp.salario);
+                            dos.writeByte(emp.control);
+                            dos.close();
+                        }
+                        
+                    } catch (IOException e) {}
+                    
+                    //mensaje de error si el id no coincide con ninguno
+                    if(!exitoBaja) System.out.println("Error, id inválido, no se realizó ninguna baja");
+                    
+                    //terminamos el caso baja
+                    break;
+                case modif: 
+                    if(!empleados.exists()) {
+                        System.out.println("Error, no existen empleados. Por favor realice al menos un alta antes de realizar una modificación.\n");
+                        break;
+                    }
+                    
+                    //leemos el archivo y almacenamos sus datos en una lista de empleados
+                    listaEmpleados = new ArrayList();
+                    try {
+                        dis = new DataInputStream(new BufferedInputStream(new FileInputStream(empleados)));
+                        while(dis.available()>0) {
+                            empleadoAux = new Empleado();
+                            empleadoAux.id = dis.readInt();
+                            empleadoAux.nombre = dis.readUTF();
+                            empleadoAux.ape1 = dis.readUTF();
+                            empleadoAux.ape2 = dis.readUTF();
+                            empleadoAux.depart = dis.readUTF();
+                            empleadoAux.ciudad = dis.readUTF();
+                            empleadoAux.salario = dis.readFloat();
+                            empleadoAux.control = dis.readByte();
+                            listaEmpleados.add(empleadoAux);
+                            System.out.println("Debug: Empleado "+empleadoAux.id+" leído");
+                        }
+                        dis.close();
+                    } catch (IOException e) {} 
+                    
+                    System.out.println("Introduzca el id del empleado a modificar:");
+                    int idModif = sc.nextInt();
+                    sc.nextLine(); //limpiamos escaner
+                    boolean exitoModif = false;
+                    
+                    // volvemos a escribir todos los empleados en el fichero, cambiando antes el byte de control del empleado que damos de baja
+                    try {
+                        dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(empleados, false))); //no utilizamos el append pues queremos sobreescribir el fichero
+                        for(Empleado emp : listaEmpleados) {
+                            Empleado empleadoModif = emp;
+                            if(emp.id==idModif) {
+                                if(emp.control == BORRADO) System.out.println("Error, empleado no existente");
+                                else{
+                                    System.out.println("Introduzca nuevo nombre empleado:");
+                                    empleadoModif.nombre = sc.nextLine();
+                                    System.out.println("Introduzca nuevo primer apellido empleado:");
+                                    empleadoModif.ape1 = sc.nextLine();
+                                    System.out.println("Introduzca nuevo segundo apellido empleado:");
+                                    empleadoModif.ape2 = sc.nextLine();
+                                    System.out.println("Introduzca nuevo departamento empleado:");
+                                    empleadoModif.depart = sc.nextLine();
+                                    System.out.println("Introduzca nuevo ciudad empleado:");
+                                    empleadoModif.ciudad = sc.nextLine();
+                                    System.out.println("Introduzca nuevo salario empleado:");
+                                    empleadoModif.salario = sc.nextFloat();
+                                    sc.nextLine(); //limpiamos scanner
+                                    
+                                    exitoModif = true;
+                                    System.out.println("Modificación realizada con éxito");
+                                }
+                            }
+                            dos.writeInt(empleadoModif.id);
+                            dos.writeUTF(empleadoModif.nombre);
+                            dos.writeUTF(empleadoModif.ape1);
+                            dos.writeUTF(empleadoModif.ape2);
+                            dos.writeUTF(empleadoModif.depart);
+                            dos.writeUTF(empleadoModif.ciudad);
+                            dos.writeFloat(empleadoModif.salario);
+                            dos.writeByte(empleadoModif.control);
+                            dos.close();
+                        }
+                        
+                    } catch (IOException e) {}
+                    
+                    if(!exitoModif) System.out.println("Error, id inválido, no se realizó ninguna modificación");
+                    
+                    //terminamos el caso modif
+                    break;
+                case consulta:
+                    if(!empleados.exists()) {
+                        System.out.println("Error, no existen empleados. Por favor realice al menos un alta antes de realizar una consulta.\n");
+                        break;
+                    }
+                    
+                    //leemos el archivo y almacenamos sus datos en una lista de empleados
+                    listaEmpleados = new ArrayList();
+                    try {
+                        dis = new DataInputStream(new BufferedInputStream(new FileInputStream(empleados)));
+                        while(dis.available()>0) {
+                            empleadoAux = new Empleado();
+                            empleadoAux.id = dis.readInt();
+                            empleadoAux.nombre = dis.readUTF();
+                            empleadoAux.ape1 = dis.readUTF();
+                            empleadoAux.ape2 = dis.readUTF();
+                            empleadoAux.depart = dis.readUTF();
+                            empleadoAux.ciudad = dis.readUTF();
+                            empleadoAux.salario = dis.readFloat();
+                            empleadoAux.control = dis.readByte();
+                            listaEmpleados.add(empleadoAux);
+                            System.out.println("Debug: Empleado "+empleadoAux.id+" leído");
+                        }
+                        dis.close();
+                    } catch (IOException e) {} 
+                    
+                    System.out.println("Introduzca el id del empleado a consultar:");
+                    int idConsulta = sc.nextInt();
+                    sc.nextLine(); //limpiamos escaner
+                    boolean exitoCons = false;
+                    
+                    // volvemos a escribir todos los empleados en el fichero, cambiando antes el byte de control del empleado que damos de baja
+                    
+                    for(Empleado emp : listaEmpleados) {
+                        if(emp.id==idConsulta) {
+                            if(emp.control == BORRADO) System.out.println("Error, empleado inexistente"); 
+                            else emp.mostrar();
+                            exitoCons = true;
+                        }
+                    }
+                    
+                    if(!exitoCons) System.out.println("Error, id inválido, no se pudo realizar consulta");
+                    
+                    //terminamos el caso consulta
                     break;
             }
         }
