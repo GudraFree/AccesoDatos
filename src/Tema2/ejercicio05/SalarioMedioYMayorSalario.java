f7cd575ac099c8d5ef66b708802767e67a408d17
GudraFree <perig1607@hotmail.com>
2017-11-15 11:42:48 +0100

    Ejercicios tema 2: 1-5

---
+++ src/Tema2/ejercicio05/SalarioMedioYMayorSalario.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tema2.ejercicio05;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Perig
+ */
+public class SalarioMedioYMayorSalario {
+    String driver = "org.sqlite.JDBC";
+    String url = "jdbc:sqlite:ejemplo.db";
+    String user = "";
+    String pass = "";
+    
+    public static void main(String[] args) {
+        SalarioMedioYMayorSalario la = new SalarioMedioYMayorSalario();
+    }
+
+    public SalarioMedioYMayorSalario() {
+        String query = "select d.dnombre, avg(e.salario), e.apellido, max(e.salario) from departamentos d, empleados e where d.dept_no=e.dept_no group by e.dept_no";
+        
+        try {
+            Class.forName(driver);
+            Connection connection = DriverManager.getConnection(url,user,pass);
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(query);
+            
+            while(result.next()) {
+                System.out.println(result.getString(1)+" "+result.getFloat(2)+", "+result.getString(3)+" "+result.getFloat(4));
+            }
+            
+            result.close();
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error, clase no encontrada");
+        } catch (SQLException e) {
+            System.out.println("Error de SQL");
+            e.printStackTrace();
+        }
+    }
+}
